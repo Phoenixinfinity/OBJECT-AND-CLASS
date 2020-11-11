a6c8addda8e04458a5ba8f0c7e68d08e7ed3b799
Phoenixinfinity <73990307+Phoenixinfinity@users.noreply.github.com>
2020-11-11 18:40:09 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class Animals
+{
+public:
+    void readAnimals()
+    {
+        cout << "\nAnimals: Cats";
+    }
+
+};
+int main()
+{
+    Animals b;
+    b.readAnimals();
+    return 0;
+}
